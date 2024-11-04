cc7aa359905d225b1397212e290ba074a6cf4f3a
kwakdeung <rhkrwnsdud22@naver.com>
2024-11-04 12:41:14 +0900

    9.4.2 상태바 색상 변경하기

--- image_carousel/lib/screen/home_screen.dart
+++ image_carousel/lib/screen/home_screen.dart
@@ -1,16 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    // 상태바 색상 변경
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
+
     return Scaffold(
+      // 페이지뷰 구현(List, Map 이용)
       body: PageView(
         children: [1, 2, 3, 4, 5]
             .map(
               (number) => Image.asset(
                 'asset/img/image_$number.jpeg',
+                // BoxFit 속성 설정
                 fit: BoxFit.cover,
               ),
             )
