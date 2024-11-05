b222796a25066ced7866c0c0fcb7d7a8b71761f9
kwakdeung <rhkrwnsdud22@naver.com>
2024-11-05 12:19:18 +0900

    10.4.1 홈 스크린 UI 구현하기

--- u_and_i/lib/screen/home_screen.dart
+++ u_and_i/lib/screen/home_screen.dart
@@ -6,7 +6,80 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Home Screen'),
+      backgroundColor: Colors.pink[100],
+      // SafeArea: 시스템 UI 피해서 UI 그리기
+      body: SafeArea(
+        top: true,
+        bottom: false,
+        child: Column(
+          // 위아래 끝에 위젯 배치
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          // 반대축 최대 크기로 늘리기
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            _DDay(),
+            _CoupleImage(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _DDay extends StatelessWidget {
+  const _DDay({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+    return Column(
+      children: [
+        const SizedBox(height: 16.0),
+        Text(
+          'U&I',
+          style: textTheme.displayLarge,
+        ),
+        const SizedBox(height: 16.0),
+        Text(
+          '우리 처음 만난 날',
+          style: textTheme.bodyLarge,
+        ),
+        Text(
+          '2021.11.23',
+          style: textTheme.bodyMedium,
+        ),
+        const SizedBox(height: 16.0),
+        IconButton(
+          onPressed: () {},
+          icon: Icon(
+            Icons.favorite,
+            color: Colors.red,
+          ),
+          iconSize: 60.0,
+        ),
+        const SizedBox(height: 16.0),
+        Text(
+          'D+365',
+          style: textTheme.displayMedium,
+        ),
+      ],
+    );
+  }
+}
+
+class _CoupleImage extends StatelessWidget {
+  const _CoupleImage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Center(
+        child: Image.asset(
+          'asset/img/middle_image.png',
+          // 화면의 반만큼 높이 구현
+          height: MediaQuery.of(context).size.height / 2,
+        ),
+      ),
     );
   }
 }
