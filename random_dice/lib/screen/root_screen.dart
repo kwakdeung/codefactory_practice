09a33ece6f0c7a44b7f279586fc425eeccf9a396
kwakdeung <rhkrwnsdud22@naver.com>
2024-11-05 14:23:33 +0900

    11.4.4 shake 플러그인 적용하기

--- random_dice/lib/screen/root_screen.dart
+++ random_dice/lib/screen/root_screen.dart
@@ -1,6 +1,9 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:random_dice/screen/home_screen.dart';
 import 'package:random_dice/screen/setting_screen.dart';
+import 'package:shake/shake.dart';
 
 class RootScreen extends StatefulWidget {
   const RootScreen({super.key});
@@ -12,6 +15,8 @@ class RootScreen extends StatefulWidget {
 class _RootScreenState extends State<RootScreen> with TickerProviderStateMixin {
   TabController? controller;
   double threshold = 2.7; // 민감도의 기본값 설정
+  int number = 1; // 주사위 숫자
+  ShakeDetector? shakeDetector;
 
   @override
   void initState() {
@@ -20,6 +25,21 @@ class _RootScreenState extends State<RootScreen> with TickerProviderStateMixin {
 
     // 컨트롤러 속성이 변경될 때 마다 실행할 함수 등록
     controller!.addListener(tabListener);
+
+    shakeDetector = ShakeDetector.autoStart(
+      shakeSlopTimeMS: 100, // 감지 주기
+      shakeThresholdGravity: threshold, // 감지 민감도
+      onPhoneShake: onPhoneShake, // 감지 후 실행할 함수
+    );
+  }
+
+  void onPhoneShake() {
+    // 감지 후 실행할 함수
+    final rand = Random();
+
+    setState(() {
+      number = rand.nextInt(5) + 1;
+    });
   }
 
   tabListener() {
@@ -30,6 +50,7 @@ class _RootScreenState extends State<RootScreen> with TickerProviderStateMixin {
   @override
   void dispose() {
     controller!.removeListener(tabListener);
+    shakeDetector!.stopListening(); // 흔들기 감지 중지
     super.dispose();
   }
 
@@ -69,7 +90,7 @@ class _RootScreenState extends State<RootScreen> with TickerProviderStateMixin {
             controller!.animateTo(index);
           });
         },
-        items: [
+        items: const [
           // 하단 탭바의 각 버튼을 구현
           BottomNavigationBarItem(
             icon: Icon(
