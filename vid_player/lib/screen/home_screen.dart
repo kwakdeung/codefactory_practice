8e52cda3880dc1305b2ee9203429cc36fea798e4
kwakdeung <rhkrwnsdud22@naver.com>
2024-11-07 18:02:19 +0900

    12.4.2 배경색 그라데이션 구현하기

--- vid_player/lib/screen/home_screen.dart
+++ vid_player/lib/screen/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:image_picker/image_picker.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -23,13 +24,37 @@ class _HomeScreenState extends State<HomeScreen> {
     // 동영상 선택 전 보여줄 위젯
     return Container(
       width: MediaQuery.of(context).size.width, // 너비 값 최대로
+      decoration: getBoxDecoration(),
       child: Column(
         // 위젯들 가운데 정렬
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          _Logo(), // 로고 이미지
+          _Logo(onTap: onNewVideoPressed), // 로고 이미지
           SizedBox(height: 30.0),
           _AppName(), // 앱 이름
+        ],
+      ),
+    );
+  }
+
+  void onNewVideoPressed() async {
+    final video = await ImagePicker().pickVideo(source: ImageSource.gallery);
+
+    if (video != null) {
+      setState(() {
+        this.video = video;
+      });
+    }
+  }
+
+  BoxDecoration getBoxDecoration() {
+    return BoxDecoration(
+      gradient: LinearGradient(
+        begin: Alignment.topCenter,
+        end: Alignment.bottomCenter,
+        colors: [
+          Color(0xFF2A3A7C),
+          Color(0xFF000118),
         ],
       ),
     );
@@ -43,12 +68,17 @@ class _HomeScreenState extends State<HomeScreen> {
 
 // 로고를 보여줄 위젯
 class _Logo extends StatelessWidget {
-  const _Logo({super.key});
+  final GestureTapCallback onTap; // 탭했을 때 실행할 함수
+
+  const _Logo({super.key, required this.onTap});
 
   @override
   Widget build(BuildContext context) {
-    return Image.asset(
-      'asset/img/logo.png', // 로고 이미지
+    return GestureDetector(
+      onTap: onTap, // 상위 위젯으로부터 탭 콜백받기
+      child: Image.asset(
+        'asset/img/logo.png', // 로고 이미지
+      ),
     );
   }
 }
