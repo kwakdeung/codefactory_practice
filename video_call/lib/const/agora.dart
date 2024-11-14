3c919cce8fc1f418b75458e2083d019776041736
kwakdeung <rhkrwnsdud22@naver.com>
2024-11-14 14:44:59 +0900

    13.2.1 아고리에서 필요한 상수값 가져오기

---
+++ video_call/lib/const/agora.dart
@@ -0,0 +1,5 @@
+// 아고라 - 영상 통화와 통화 기능 API 유료 서비스(유료 서비스지만 회원가입을 하면 매달 1만 분을 무료로 사용 가능)
+// 개인 APP_ID, CHANNEL_NAME, TEMP_TOKEN 넣어 사용하기
+const APP_ID = '앱ID를 입력해주세요!!!';
+const CHANNEL_NAME = '채널 이름을 입력해주세요!!!';
+const TEMP_TOKEN = '토큰값을 입력해주세요!!!';
