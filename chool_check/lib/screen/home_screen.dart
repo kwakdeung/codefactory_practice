015c328648215640b2734d8b815771760a0c06f3
kwakdeung <rhkrwnsdud22@naver.com>
2024-11-18 10:35:46 +0900

    14.4.2 Body 구현하기

--- chool_check/lib/screen/home_screen.dart
+++ chool_check/lib/screen/home_screen.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomeScreen extends StatelessWidget {
+  // 지도 초기화 위치
+  static const LatLng companyLatLng = LatLng(
+    37.5233273, // 위도
+    126.921252, // 경도
+  );
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: renderAppBar(),
-      body: Text('Home Screen'),
+      body: const GoogleMap(
+        // 지도 위치 지정
+        initialCameraPosition: CameraPosition(
+          target: companyLatLng,
+          zoom: 16, // 확대 정도 (값이 높을수록 크게 보임)
+        ),
+      ),
     );
   }
 }
