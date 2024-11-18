3822f3320ca98480b61db654fd0baeb819558a03
kwakdeung <rhkrwnsdud22@naver.com>
2024-11-18 11:00:48 +0900

    14.4.4 위치 권한 관리하기

--- chool_check/lib/screen/home_screen.dart
+++ chool_check/lib/screen/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -7,45 +8,101 @@ class HomeScreen extends StatelessWidget {
     37.5233273, // 위도
     126.921252, // 경도
   );
+
+  // renderAppBar() 함수 아래에 입력하기
+  Future<String> checkPermission() async {
+    final isLocationEnabled = await Geolocator.isLocationServiceEnabled();
+    // 위치 서비스 활성화 여부 확인
+    if (!isLocationEnabled) {
+      // 위치 서비스 활성화 안 됐을 때
+      return '위치 서비스를 활성화해주세요.';
+    }
+
+    LocationPermission checkedPermission =
+        await Geolocator.checkPermission(); // 위치 권한 확인
+
+    // 위치 권한 거절됨
+    if (checkedPermission == LocationPermission.denied) {
+      // 위치 권한 요청하기
+      checkedPermission = await Geolocator.requestPermission();
+
+      if (checkedPermission == LocationPermission.denied) {
+        return '위치 권한을 허가해주세요.';
+      }
+    }
+
+    // 위치 권한 거절됨 (앱에서 재요청 불가)
+    if (checkedPermission == LocationPermission.deniedForever) {
+      return '앱의 위치 권한을 설정에서 허가해주세요.';
+    }
+
+    // 위 모든 조건이 통과되면 위치 권한 허가 완료
+    return '위치 권한이 허가 되었습니다.';
+  }
+
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: renderAppBar(),
-      body: Column(
-        children: [
-          // 2/3 만큼 공간 차지
-          const Expanded(
-            flex: 2,
-            child: GoogleMap(
-              // 지도 위치 지정
-              initialCameraPosition: CameraPosition(
-                target: companyLatLng,
-                zoom: 16, // 확대 정도 (값이 높을수록 크게 보임)
-              ),
+      body: FutureBuilder<String>(
+        future: checkPermission(),
+        builder: (context, snapshot) {
+          // 로딩 상태
+          if (!snapshot.hasData &&
+              snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          // 권한 허가된 상태
+          if (snapshot.data == '위치 권한이 허가 되었습니다.') {
+            return Column(
+              children: [
+                // 2/3 만큼 공간 차지
+                const Expanded(
+                  flex: 2,
+                  child: GoogleMap(
+                    // 지도 위치 지정
+                    initialCameraPosition: CameraPosition(
+                      target: companyLatLng,
+                      zoom: 16, // 확대 정도 (값이 높을수록 크게 보임)
+                    ),
+                  ),
+                ),
+                // 1/3 만큼 공간 차지
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      // 시계 아이콘
+                      const Icon(
+                        Icons.timelapse_outlined,
+                        color: Colors.blue,
+                        size: 50.0,
+                      ),
+                      const SizedBox(height: 20.0),
+                      // 출근하기 버튼
+                      ElevatedButton(
+                        onPressed: () {},
+                        child: const Text('출근하기!'),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            );
+          }
+
+          // 권한 없는 상태
+          return Center(
+            child: Text(
+              snapshot.data.toString(),
             ),
-          ),
-          // 1/3 만큼 공간 차지
-          Expanded(
-              child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              // 시계 아이콘
-              const Icon(
-                Icons.timelapse_outlined,
-                color: Colors.blue,
-                size: 50.0,
-              ),
-              const SizedBox(height: 20.0),
-              // 출근하기 버튼
-              ElevatedButton(
-                onPressed: () {},
-                child: const Text('출근하기!'),
-              ),
-            ],
-          )),
-        ],
+          );
+        },
       ),
     );
   }
