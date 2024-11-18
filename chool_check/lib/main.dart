d7e3371a708aebbdfd38f481b884be35290e9328
kwakdeung <rhkrwnsdud22@naver.com>
2024-11-18 10:27:55 +0900

    14.2.4 프로젝트 초기화하기

--- chool_check/lib/main.dart
+++ chool_check/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:chool_check/screen/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_config/flutter_config.dart';
 
@@ -5,21 +6,14 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized(); // Required by FlutterConfig
   await FlutterConfig.loadEnvVariables();
 
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  runApp(
+    MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+      home: const HomeScreen(),
+    ),
+  );
 }
