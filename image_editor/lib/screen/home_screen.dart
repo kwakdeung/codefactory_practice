21320a9930015c01be0fdb2e889b355e111c69c6
kwakdeung <rhkrwnsdud22@naver.com>
2024-11-20 10:13:18 +0900

    15.4.2 Image 선택하기

--- image_editor/lib/screen/home_screen.dart
+++ image_editor/lib/screen/home_screen.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:image_editor/component/main_app_bar.dart';
+import 'package:image_picker/image_picker.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  XFile? image; // 선택한 이미지를 저장할 변수
 
   @override
   Widget build(BuildContext context) {
@@ -24,7 +32,16 @@ class HomeScreen extends StatelessWidget {
     );
   }
 
-  void onPickImage() {}
+  // 미리 생성해둔 onPickImage() 함수 변경하기
+  void onPickImage() async {
+    // 갤러리에서 이미지를 선택하기
+    final image = await ImagePicker().pickImage(source: ImageSource.gallery);
+
+    // 선택한 이미지 저장하기
+    setState(() {
+      this.image = image;
+    });
+  }
 
   void onSaveImage() {}
 
