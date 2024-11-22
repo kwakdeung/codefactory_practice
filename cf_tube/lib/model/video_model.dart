44b891a82bdce649040a9ac87c54f37df42344f1
kwakdeung <rhkrwnsdud22@naver.com>
2024-11-22 21:01:23 +0900

    16.4.1 VideoModel 구현하기
    유튜브 API 정보 가져오기
    - 동영상 ID
    - 동영상 제목

---
+++ cf_tube/lib/model/video_model.dart
@@ -0,0 +1,10 @@
+// 유튜브 API 정보 - 동영상 ID, 동영상 제목
+class VideoModel {
+  final String id; // 동영상 ID
+  final String title; // 동영상 제목
+
+  VideoModel({
+    required this.id,
+    required this.title,
+  });
+}
