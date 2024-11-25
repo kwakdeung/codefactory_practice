9228c5fa21a388ccd71dd347e1dbeae0005421e4
kwakdeung <rhkrwnsdud22@naver.com>
2024-11-25 15:15:24 +0900

    16.4.4 ListView 구현하기

--- cf_tube/lib/repository/youtube_repository.dart
+++ cf_tube/lib/repository/youtube_repository.dart
@@ -3,11 +3,11 @@ import 'package:dio/dio.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class YoutubeRepository {
-  String apiKey = dotenv.env["API_KEY"]!;
-  String youtubeApiBaseUrl = dotenv.env["YOUTUBE_API_BASE_URL"]!;
-  String cfChannelId = dotenv.env["CF_CHANNEL_ID"]!;
+  static String apiKey = dotenv.env["API_KEY"]!;
+  static String youtubeApiBaseUrl = dotenv.env["YOUTUBE_API_BASE_URL"]!;
+  static String cfChannelId = dotenv.env["CF_CHANNEL_ID"]!;
 
-  Future<List<VideoModel>> getVideos() async {
+  static Future<List<VideoModel>> getVideos() async {
     final resp = await Dio().get(
       // GET 메서드 보내기
       youtubeApiBaseUrl, // 요청을 보낼 URL
@@ -19,7 +19,14 @@ class YoutubeRepository {
         'order': 'date',
       },
     );
-    final listWithData = resp.data['items'].where(
+
+    // 'items'를 List<dynamic>으로 변환
+    final items = (resp.data['items'] as List<dynamic>?);
+    if (items == null) {
+      throw Exception('items가 null입니다.');
+    }
+
+    final listWithData = items.where(
       (item) =>
           item?['id']?['videoId'] != null && item?['snippet']?['title'] != null,
     ); // videoId와 title이 null이 아닌 값들만 필터링
--- cf_tube/lib/screen/home_screen.dart
+++ cf_tube/lib/screen/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:cf_tube/component/custom_youtube_player.dart';
 import 'package:cf_tube/model/video_model.dart';
+import 'package:cf_tube/repository/youtube_repository.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -9,11 +10,43 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: CustomYoutubePlayer(
-        videoModel: VideoModel(
-          id: '3Ck42C2ZCb8', // 샘플 동영상 ID
-          title: '다트 언어 기본기 1시간만에 끝내기', // 샘플 제목
-        ),
+      appBar: AppBar(
+        centerTitle: true, // 제목 가운데 정렬
+        title: const Text('코팩튜브'),
+        backgroundColor: Colors.black,
+      ),
+      body:
+          // CustomYoutubePlayer(
+          //   videoModel: VideoModel(
+          //     id: '3Ck42C2ZCb8', // 샘플 동영상 ID
+          //     title: '다트 언어 기본기 1시간만에 끝내기', // 샘플 제목
+          //   ),
+          // ),
+          FutureBuilder<List<VideoModel>>(
+        future: YoutubeRepository.getVideos(),
+        builder: ((context, snapshot) {
+          // 에러가 있을 경우 에러 화면 표시하기
+          if (snapshot.hasError) {
+            return Center(
+              child: Text(
+                snapshot.error.toString(),
+              ),
+            );
+          }
+          // 로딩 중일 때 로딩 위젯 보여주기
+          if (!snapshot.hasData) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          return ListView(
+            physics: const BouncingScrollPhysics(),
+            children: snapshot.data!
+                .map((e) => CustomYoutubePlayer(videoModel: e))
+                .toList(),
+          );
+        }),
       ),
     );
   }
