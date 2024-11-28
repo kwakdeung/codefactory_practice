48351aec8bc1e364719802827dbf34e50443443a
kwakdeung <rhkrwnsdud22@naver.com>
2024-11-28 13:27:54 +0900

    18.2.4 드리프트 초기화하기

--- calendar_scheduler/lib/main.dart
+++ calendar_scheduler/lib/main.dart
@@ -1,11 +1,17 @@
+import 'package:calendar_scheduler/database/drift_database.dart';
 import 'package:calendar_scheduler/screen/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await initializeDateFormatting(); // intl 패키지 초기화(다국어화)
+
+  final database = LocalDatabase(); // 데이터베이스 생성
+
+  GetIt.I.registerSingleton<LocalDatabase>(database); // GetIt에 데이터베이스 변수 주입하기
 
   runApp(
     const MaterialApp(
